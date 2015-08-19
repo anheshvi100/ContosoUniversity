eed749968f77525b45d366c7d558e1ab077f5b84
anheshvi100 <hemakanagaraj@gmail.com>
2015-08-19 06:42:40 -0400

    edit method

--- Controllers/InstructorController.cs
+++ Controllers/InstructorController.cs
@@ -9,6 +9,7 @@ using System.Web.Mvc;
 using ContosoUniversity.DAL;
 using ContosoUniversity.Models;
 using ContosoUniversity.ViewModels;
+using System.Data.Entity.Infrastructure;
 namespace ContosoUniversity.Controllers
 {
     public class InstructorController : Controller
@@ -30,14 +31,23 @@ namespace ContosoUniversity.Controllers
                 viewModel.Courses = viewModel.Instructors.Where(
                     i => i.ID == id.Value).Single().Courses;
             }
-
             if (courseID != null)
             {
                 ViewBag.CourseID = courseID.Value;
-                viewModel.Enrollments = viewModel.Courses.Where(
-                    x => x.CourseID == courseID).Single().Enrollments;
-            }
+                // Lazy loading
+                //viewModel.Enrollments = viewModel.Courses.Where(
+                //    x => x.CourseID == courseID).Single().Enrollments;
+                // Explicit loading
+                var selectedCourse = viewModel.Courses.Where(x => x.CourseID == courseID).Single();
+                db.Entry(selectedCourse).Collection(x => x.Enrollments).Load();
+                foreach (Enrollment enrollment in selectedCourse.Enrollments)
+                {
+                    db.Entry(enrollment).Reference(x => x.Student).Load();
+                }
 
+                viewModel.Enrollments = selectedCourse.Enrollments;
+            }
+          
             return View(viewModel);
         }
 
@@ -82,54 +92,268 @@ namespace ContosoUniversity.Controllers
         }
 
         // GET: Instructor/Edit/5
+
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Instructor instructor = db.Instructors.Find(id);
+            Instructor instructor = db.Instructors
+                .Include(i => i.OfficeAssignment)
+                .Include(i => i.Courses)
+                .Where(i => i.ID == id)
+                .Single();
+            PopulateAssignedCourseData(instructor);
             if (instructor == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.ID = new SelectList(db.OfficeAssignments, "InstructorID", "Location", instructor.ID);
             return View(instructor);
+        }
+
+        private void PopulateAssignedCourseData(Instructor instructor)
+        {
+            var allCourses = db.Courses;
+            var instructorCourses = new HashSet<int>(instructor.Courses.Select(c => c.CourseID));
+            var viewModel = new List<AssignedCourseData>();
+            foreach (var course in allCourses)
+            {
+                viewModel.Add(new AssignedCourseData
+                {
+                    CourseID = course.CourseID,
+                    Title = course.Title,
+                    Assigned = instructorCourses.Contains(course.CourseID)
+                });
+            }
+            ViewBag.Courses = viewModel;
         }
 
         // POST: Instructor/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+      
+        //[HttpPost, ActionName("Edit")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult EditPost(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    var instructorToUpdate = db.Instructors
+        //       .Include(i => i.OfficeAssignment)
+        //       .Where(i => i.ID == id)
+        //       .Single();
+
+        //    if (TryUpdateModel(instructorToUpdate, "",
+        //       new string[] { "LastName", "FirstMidName", "HireDate", "OfficeAssignment" }))
+        //    {
+        //        try
+        //        {
+        //            if (String.IsNullOrWhiteSpace(instructorToUpdate.OfficeAssignment.Location))
+        //            {
+        //                instructorToUpdate.OfficeAssignment = null;
+        //            }
+
+        //            db.SaveChanges();
+
+        //            return RedirectToAction("Index");
+        //        }
+        //        catch (RetryLimitExceededException /* dex */)
+        //        {
+        //            //Log the error (uncomment dex variable name and add a line here to write a log.
+        //            ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+        //        }
+        //    }
+        //    return View(instructorToUpdate);
+        //}
+        //public ActionResult EditPost(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    var instructorToUpdate = db.Instructors
+        //       .Include(i => i.OfficeAssignment)
+        //       .Where(i => i.ID == id)
+        //       .Single();
+
+        //    if (TryUpdateModel(instructorToUpdate, "",
+        //       new string[] { "LastName", "FirstMidName", "HireDate", "OfficeAssignment" }))
+        //    {
+        //        try
+        //        {
+        //            if (String.IsNullOrWhiteSpace(instructorToUpdate.OfficeAssignment.Location))
+        //            {
+        //                instructorToUpdate.OfficeAssignment = null;
+        //            }
+
+        //            db.SaveChanges();
+
+        //            return RedirectToAction("Index");
+        //        }
+        //        catch (RetryLimitExceededException /* dex */)
+        //        {
+        //            //Log the error (uncomment dex variable name and add a line here to write a log.
+        //            ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+        //        }
+        //    }
+        //    return View(instructorToUpdate);
+        //}
+        //// GET: Instructor/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Instructor instructor = db.Instructors.Find(id);
+        //    if (instructor == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(instructor);
+        //}
+        //[HttpPost]
+
+        //[ValidateAntiForgeryToken]
+
+        //public ActionResult Edit(int? id, string[] selectedCourses)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    var instructorToUpdate = db.Instructors
+        //       .Include(i => i.OfficeAssignment)
+        //       .Include(i => i.Courses)
+        //       .Where(i => i.ID == id)
+        //       .Single();
+
+        //    if (TryUpdateModel(instructorToUpdate, "",
+        //       new string[] { "LastName", "FirstMidName", "HireDate", "OfficeAssignment" }))
+        //    {
+        //        try
+        //        {
+        //            if (String.IsNullOrWhiteSpace(instructorToUpdate.OfficeAssignment.Location))
+        //            {
+        //                instructorToUpdate.OfficeAssignment = null;
+        //            }
+
+        //            UpdateInstructorCourses(selectedCourses, instructorToUpdate);
+
+        //            db.SaveChanges();
+
+        //            return RedirectToAction("Index");
+        //        }
+        //        catch (RetryLimitExceededException /* dex */)
+        //        {
+        //            //Log the error (uncomment dex variable name and add a line here to write a log.
+        //            ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+        //        }
+        //    }
+        //    PopulateAssignedCourseData(instructorToUpdate);
+        //    return View(instructorToUpdate);
+        //}
+        //private void UpdateInstructorCourses(string[] selectedCourses, Instructor instructorToUpdate)
+        //{
+        //    if (selectedCourses == null)
+        //    {
+        //        instructorToUpdate.Courses = new List<Course>();
+        //        return;
+        //    }
+
+        //    var selectedCoursesHS = new HashSet<string>(selectedCourses);
+        //    var instructorCourses = new HashSet<int>
+        //        (instructorToUpdate.Courses.Select(c => c.CourseID));
+        //    foreach (var course in db.Courses)
+        //    {
+        //        if (selectedCoursesHS.Contains(course.CourseID.ToString()))
+        //        {
+        //            if (!instructorCourses.Contains(course.CourseID))
+        //            {
+        //                instructorToUpdate.Courses.Add(course);
+        //            }
+        //        }
+        //        else
+        //        {
+        //            if (instructorCourses.Contains(course.CourseID))
+        //            {
+        //                instructorToUpdate.Courses.Remove(course);
+        //            }
+        //        }
+        //    }
+        //}
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,LastName,FirstMidName,HireDate")] Instructor instructor)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(instructor).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.ID = new SelectList(db.OfficeAssignments, "InstructorID", "Location", instructor.ID);
-            return View(instructor);
-        }
-
-        // GET: Instructor/Delete/5
-        public ActionResult Delete(int? id)
+        public ActionResult Edit(int? id, string[] selectedCourses)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Instructor instructor = db.Instructors.Find(id);
-            if (instructor == null)
-            {
-                return HttpNotFound();
-            }
-            return View(instructor);
-        }
+            var instructorToUpdate = db.Instructors
+               .Include(i => i.OfficeAssignment)
+               .Include(i => i.Courses)
+               .Where(i => i.ID == id)
+               .Single();
 
-        // POST: Instructor/Delete/5
+            if (TryUpdateModel(instructorToUpdate, "",
+               new string[] { "LastName", "FirstMidName", "HireDate", "OfficeAssignment" }))
+            {
+                try
+                {
+                    if (String.IsNullOrWhiteSpace(instructorToUpdate.OfficeAssignment.Location))
+                    {
+                        instructorToUpdate.OfficeAssignment = null;
+                    }
+
+                    UpdateInstructorCourses(selectedCourses, instructorToUpdate);
+
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                catch (RetryLimitExceededException /* dex */)
+                {
+                    //Log the error (uncomment dex variable name and add a line here to write a log.
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+            }
+            PopulateAssignedCourseData(instructorToUpdate);
+            return View(instructorToUpdate);
+        }
+        private void UpdateInstructorCourses(string[] selectedCourses, Instructor instructorToUpdate)
+        {
+            if (selectedCourses == null)
+            {
+                instructorToUpdate.Courses = new List<Course>();
+                return;
+            }
+
+            var selectedCoursesHS = new HashSet<string>(selectedCourses);
+            var instructorCourses = new HashSet<int>
+                (instructorToUpdate.Courses.Select(c => c.CourseID));
+            foreach (var course in db.Courses)
+            {
+                if (selectedCoursesHS.Contains(course.CourseID.ToString()))
+                {
+                    if (!instructorCourses.Contains(course.CourseID))
+                    {
+                        instructorToUpdate.Courses.Add(course);
+                    }
+                }
+                else
+                {
+                    if (instructorCourses.Contains(course.CourseID))
+                    {
+                        instructorToUpdate.Courses.Remove(course);
+                    }
+                }
+            }
+        }
+        //// POST: Instructor/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
