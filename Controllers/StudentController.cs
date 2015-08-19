8cee02ad153e78c2881073e06da850428bd39390
anheshvi100 <hemakanagaraj@gmail.com>
2015-08-19 02:54:50 -0400

    delete

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -128,11 +128,10 @@ namespace ContosoUniversity.Controllers
             }
             return View(student);
         }
-
         // POST: Students/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult Delete(int id)
         {
             try
             {
